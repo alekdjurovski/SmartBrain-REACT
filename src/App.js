7ccdf97f52a4dbcbe8ae8dc0526e7af39e49aaf5
alek.djurovski <alek.djurovski@gmail.com>
2018-11-18 21:35:33 +0100

    start react project smart brain

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Navigation from './components/Navigation/Navigation';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        {/*<Logo />
+        <ImageLinkForm />
+        <FaceRecognition /> */ }
+      </div>
+    );
+  }
+}
+
+export default App;
