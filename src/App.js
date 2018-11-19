c173e4664abf5fc6c39af4c25fb60bb96970a29b
alek.djurovski <alek.djurovski@gmail.com>
2018-11-19 01:39:48 +0100

    create background gradient and navigation component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import './App.css';
 
+
 class App extends Component {
   render() {
     return (
