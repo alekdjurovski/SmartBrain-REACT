6dbc660720bd72b35a984ff9391f2dd11ffaab35
alek.djurovski <alek.djurovski@gmail.com>
2018-11-21 01:16:38 +0100

    some design change

--- src/App.js
+++ src/App.js
@@ -6,25 +6,26 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Particles from 'react-particles-js';
 import './App.css';
 
+const particlesOpt = {
+            particles: {
+              line_linked: {
+                shadow: {
+                  enable: true,
+                  color: "#3CA9D1",
+                  blur: 5
+                }
+              }
+            }
+          }
 
+          
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Particles className='particles'
-        params={{
-          particles: {
-            line_linked: {
-              shadow: {
-                enable: true,
-                color: "#3CA9D1",
-                blur: 5
-               }
-              }
-                }
-            }}
-            
-            />
+        <Particles className='particles'
+          params={particlesOpt}              
+        />
         <Navigation />
         <Logo />
         <Rank />
